a892dd122f0d32798105ccafd02aabeae321e072
mariegoltermann <mari09a7@stud.kea.dk>
2022-09-13 12:27:57 +0200

    Co-authored-by: char308z <char308z@users.noreply.github.com>

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
+     
+      fontFamily:{
+        display: 'aktiv-grotesk-extended'
+      },
       colors:{
         pink:'#F20FB6',
         green:'#76D930'
